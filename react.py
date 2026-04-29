00e3faf54d837768802b5b6cd3029ee4c5e476aa
Moonight0 <moonightgamer12@gmail.com>
2026-04-29 14:23:23 -0400

    Add files via upload

---
+++ react.py
@@ -0,0 +1,17 @@
+import pyautogui
+import keyboard
+import time
+
+cor = []
+while True:
+    if keyboard.is_pressed('enter'):
+        print(pyautogui.position())
+        
+        
+    
+    ##cor = 
+#cor verde
+    ##if cor == [62, 222, 105]:
+       ## print('verde massa')
+x577
+y304
